26bb7cd466fc8e0408d9f3ca2d829976ec50b26c
Анна Унру <a.unru@2gis.ru>
2016-04-12 16:00:55 +0600

    Задание с первой лекции. Черново

---
+++ Console1/Console1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    string a = Console.ReadLine();
+        //    string b = Console.ReadLine();
+        //    int sum = Convert.ToInt32(a) + Convert.ToInt32(b);
+        //    Console.WriteLine(sum);
+        //    Console.ReadLine();
+
+            //string n = Console.ReadLine();
+            //string i = Console.ReadLine();
+
+            //byte N = Convert.ToByte(n);
+            //byte I = Convert.ToByte(i);
+
+            //int r;
+
+            //r = (N >> I) & 1;
+            //Console.WriteLine(r);
+            //Console.ReadLine();
+
+            string n = Console.ReadLine();
+            
+            byte N = Convert.ToByte(n);
+           
+            int r;
+
+            r = (N>>(N-1)) & 0;
+            Console.WriteLine(r);
+            
+        }
+    }
+}
