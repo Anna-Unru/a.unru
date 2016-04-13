88700008f56f16072d0e70da650ce448225e2382
Анна Унру <a.unru@2gis.ru>
2016-04-13 09:12:29 +0600

    Три задания с лекции_1.

---
+++ Lection1/Lection1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lection1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1
+            // Ввести с консоли 2 числа. Вывести их сумму на консоль
+            //string a = Console.ReadLine();
+            //string b = Console.ReadLine();
+            //int sum = Convert.ToInt32(a) + Convert.ToInt32(b);
+            //Console.WriteLine(sum);
+
+            // Задание 2
+            // Ввести с консоли 2 числа: n и i. Вывести значение i-го бита числа n
+            //string n = Console.ReadLine();
+            //string i = Console.ReadLine();
+
+            //byte N = Convert.ToByte(n);
+            //byte I = Convert.ToByte(i);
+
+            //int r;
+
+            //r = (N >> I) & 1;
+            //Console.WriteLine(r);
+
+            // Задание 3
+            // Ввести с консоли число. Обнулить последний бит этого числа.
+            // Вывести на консоль
+            //string n = Console.ReadLine();
+
+            //byte N = Convert.ToByte(n);
+
+            //int r;
+            //r = N & 0xFFFE;
+            //Console.WriteLine(r);
+        }
+    }
+}
